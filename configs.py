2d7a946f83f290a92c6cee7048f49f22acd195cc
alexhart23 <alexhart23@gmail.com>
2015-11-13 20:19:27 -0500

    Adding player data pull and splitting configs into another file

---
+++ configs.py
@@ -0,0 +1,9 @@
+__author__ = 'Alex Hart'
+
+season_id = '348' # 348 for 2015 NFL
+league_id = 'leagueid' # check the url on your league page
+year = '2015'
+league_key = season_id+".l."+league_id
+
+OAUTH_CONSUMER_KEY = 'yahoo app key'
+OAUTH_SHARED_SECRET = 'yahoo app secret'
--- download_data.py
+++ download_data.py
@@ -1,21 +1,17 @@
+__author__ = 'Alex Hart'
+
 from rauth import OAuth1Service
 import json
 import csv
-
-season_id = 'add season id' # 348 for 2015 NFL
-league_id = 'add league id' # check the url on your league page
-league_key = season_id+".l."+league_id
+import configs
 
 def login():
-    OAUTH_CONSUMER_KEY = ' add consumer key'
-    OAUTH_SHARED_SECRET = 'add secret key'
-
     request_token_url = 'https://api.login.yahoo.com/oauth/v2/get_request_token'
     authorize_url = 'https://api.login.yahoo.com/oauth/v2/request_auth'
     access_token_url = 'https://api.login.yahoo.com/oauth/v2/get_token'
 
-    yahoo = OAuth1Service(consumer_key=OAUTH_CONSUMER_KEY,\
-                          consumer_secret=OAUTH_SHARED_SECRET,\
+    yahoo = OAuth1Service(consumer_key=configs.OAUTH_CONSUMER_KEY,\
+                          consumer_secret=configs.OAUTH_SHARED_SECRET,\
                           name='yahoo',\
                           access_token_url=access_token_url,\
                           authorize_url=authorize_url,\
@@ -41,18 +37,13 @@ def login():
 
 session = login()
 
-# http://fantasysports.yahooapis.com/fantasy/v2/league/348.l.141287/players
-# player name
-# position
-# team
-# player key
 # age
 
 # ROS projected points
 
 def get_league_info(session):
     print("Getting league info...")
-    url = 'http://fantasysports.yahooapis.com/fantasy/v2/leagues;league_keys={league_key}/settings'.format(league_key=league_key)
+    url = 'http://fantasysports.yahooapis.com/fantasy/v2/leagues;league_keys={league_key}/settings'.format(league_key=configs.league_key)
     json_string = session.get(url, params={'format': 'json'}).content
     parsed_json = json.loads(json_string.decode('utf8'))
     num_teams = parsed_json['fantasy_content']['leagues']['0']['league'][0]['num_teams']
@@ -71,13 +62,13 @@ def get_league_info(session):
 # this only needs to be run once after the draft
 def get_auction_values(session):
     print("Getting auction values...")
-    url = 'http://fantasysports.yahooapis.com/fantasy/v2/leagues;league_keys={league_key}/draftresults'.format(league_key=league_key)
+    url = 'http://fantasysports.yahooapis.com/fantasy/v2/leagues;league_keys={league_key}/draftresults'.format(league_key=configs.league_key)
     json_string = session.get(url, params={'format': 'json'}).content
     parsed_json = json.loads(json_string.decode('utf8'))
     #print(parsed_json['fantasy_content']['leagues']['0']['league'][1]['draft_results']['170']['draft_result']['player_key'])
     num_drafted = parsed_json['fantasy_content']['leagues']['0']['league'][1]['draft_results']['count']
 
-    with open('auction_values.{league_key}.csv'.format(league_key=league_key),'w', newline='') as csvfile:
+    with open('auction_values.{league_key}.csv'.format(league_key=configs.league_key),'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
         writer.writerow(['player_key','cost'])
         for i in range(0,num_drafted):
@@ -87,12 +78,12 @@ def get_auction_values(session):
 
 def get_current_rosters(session, num_teams):
     print("Getting current rosters...")
-    with open('current_rosters.{league_key}.csv'.format(league_key=league_key),'w', newline='') as csvfile:
+    with open('current_rosters.{league_key}.csv'.format(league_key=configs.league_key),'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
         writer.writerow(['player_key','team_key'])
         # not starting with 0 in range, so need to add 1 to total teams
         for team in range(1,num_teams+1):
-            url = 'http://fantasysports.yahooapis.com/fantasy/v2/teams;team_keys={league_key}.t.{team}/roster'.format(league_key=league_key,team=str(team))
+            url = 'http://fantasysports.yahooapis.com/fantasy/v2/teams;team_keys={league_key}.t.{team}/roster'.format(league_key=configs.league_key,team=str(team))
             json_string = session.get(url, params={'format': 'json'}).content
             parsed_json = json.loads(json_string.decode('utf8'))
             # need team_key, player_key, first, last, editorial_team_abbr, display_position, eligible_positions
@@ -107,12 +98,12 @@ def get_current_rosters(session, num_teams):
 
 def get_managers(session, num_teams):
     print("Getting manager info...")
-    with open('managers.{league_key}.csv'.format(league_key=league_key),'w', newline='') as csvfile:
+    with open('managers.{league_key}.csv'.format(league_key=configs.league_key),'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
         writer.writerow(['team_key','team_name','manager'])
         # not starting with 0 in range, so need to add 1 to total teams
         for team in range(1,num_teams+1):
-            url = 'http://fantasysports.yahooapis.com/fantasy/v2/teams;team_keys={league_key}.t.{team}'.format(league_key=league_key,team=str(team))
+            url = 'http://fantasysports.yahooapis.com/fantasy/v2/teams;team_keys={league_key}.t.{team}'.format(league_key=configs.league_key,team=str(team))
             json_string = session.get(url, params={'format': 'json'}).content
             parsed_json = json.loads(json_string.decode('utf8'))
             team_key = parsed_json['fantasy_content']['teams']['0']['team'][0][0]['team_key']
@@ -122,17 +113,40 @@ def get_managers(session, num_teams):
 
 def get_stats(session):
     print("Getting YTD points...")
-    with open('season_stats.{league_key}.csv'.format(league_key=league_key),'w', newline='') as csvfile:
+    with open('season_stats.{league_key}.csv'.format(league_key=configs.league_key),'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
         writer.writerow(['player_key','points'])
-        rows = csv.DictReader(open('current_rosters.{league_key}.csv'.format(league_key=league_key)))
+        rows = csv.DictReader(open('current_rosters.{league_key}.csv'.format(league_key=configs.league_key)))
         for row in rows:
             player_key = row['player_key']
-            url = 'http://fantasysports.yahooapis.com/fantasy/v2/league/{league_key}/players;player_keys={player_key}/stats'.format(league_key=league_key,player_key=player_key)
+            url = 'http://fantasysports.yahooapis.com/fantasy/v2/league/{league_key}/players;player_keys={player_key}/stats'.format(league_key=configs.league_key,player_key=player_key)
             json_string = session.get(url, params={'format': 'json'}).content
             parsed_json = json.loads(json_string.decode('utf8'))
             points = parsed_json['fantasy_content']['league'][1]['players']['0']['player'][1]['player_points']['total']
             writer.writerow([player_key,points])
+
+def get_player_info(session):
+    print("Getting player info...")
+    with open('player_info.{league_key}.csv'.format(league_key=configs.league_key),'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        writer.writerow(['player_key','last_name','first_name','pos','team'])
+        rows = csv.DictReader(open('current_rosters.{league_key}.csv'.format(league_key=configs.league_key)))
+        for row in rows:
+            player_key = row['player_key']
+            url = 'http://fantasysports.yahooapis.com/fantasy/v2/player/{player_key}'.format(player_key=player_key)
+            json_string = session.get(url, params={'format': 'json'}).content
+            parsed_json = json.loads(json_string.decode('utf8'))
+            player_key = parsed_json['fantasy_content']['player'][0][0]['player_key']
+            last_name = parsed_json['fantasy_content']['player'][0][2]['name']['last']
+            first_name = parsed_json['fantasy_content']['player'][0][2]['name']['first']
+            for i in parsed_json['fantasy_content']['player'][0]:
+                if 'display_position' in i:
+                    index = parsed_json['fantasy_content']['player'][0].index(i)
+                    pos = parsed_json['fantasy_content']['player'][0][index]['display_position']
+                elif 'editorial_team_abbr' in i:
+                    index = parsed_json['fantasy_content']['player'][0].index(i)
+                    team = parsed_json['fantasy_content']['player'][0][index]['editorial_team_abbr']
+            writer.writerow([player_key,last_name,first_name,pos,team])
 
 num_teams, positions, num_roster_spots = get_league_info(session)
 get_managers(session,num_teams)
@@ -141,7 +155,14 @@ get_managers(session,num_teams)
 get_auction_values(session)
 
 get_current_rosters(session,num_teams)
+get_player_info(session)
 get_stats(session)
+
+
+
+
+
+
 
 
 
