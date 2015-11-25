f59ba9a6db97b6f5684ae6031f24f161bc1f67ab
alexhart23 <alexhart23@users.noreply.github.com>
2015-11-25 15:11:16 -0500

    Adding config for number of years in future
    Add a config for telling the keeper value compiler how many years to generate for

--- configs.py
+++ configs.py
@@ -5,5 +5,8 @@ league_id = 'leagueid' # check the url on your league page
 year = '2015'
 league_key = season_id+".l."+league_id
 
+# how many years we want to calculate keeper values for
+years_in_future = 5
+
 OAUTH_CONSUMER_KEY = 'yahoo app key'
 OAUTH_SHARED_SECRET = 'yahoo app secret'
